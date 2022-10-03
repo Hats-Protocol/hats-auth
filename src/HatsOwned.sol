c9a9017431c1609336c946c5d3496674e2158dd3
spengrah <code@spengrah.xyz>
2022-10-03 15:15:10 -0500

    factor out common logic

--- src/HatsOwned.sol
+++ src/HatsOwned.sol
@@ -2,33 +2,12 @@
 pragma solidity >=0.8.13;
 
 import "./Interfaces/IHats.sol";
+import "./HatsOwnedCommon.sol";
 
 /// @notice Single owner authorization mixin using Hats Protocol
-/// @dev Forked from solmate (https://github.com/transmissions11/solmate/blob/main/src/auth/Owned.sol)
+/// @dev For inherentence into contracts not deployed as proxies. Forked from solmate (https://github.com/transmissions11/solmate/blob/main/src/auth/Owned.sol).
 /// @author Hats Protocol
-abstract contract HatsOwned {
-    /*//////////////////////////////////////////////////////////////
-                                 EVENTS
-    //////////////////////////////////////////////////////////////*/
-
-    event OwnerHatUpdated(
-        uint256 indexed ownerHat,
-        address indexed hatsAddress
-    );
-
-    /*//////////////////////////////////////////////////////////////
-                            OWNERSHIP STORAGE
-    //////////////////////////////////////////////////////////////*/
-
-    IHats internal HATS;
-    uint256 public ownerHat;
-
-    modifier onlyOwner() virtual {
-        require(HATS.isWearerOfHat(msg.sender, ownerHat), "UNAUTHORIZED");
-
-        _;
-    }
-
+abstract contract HatsOwned is HatsOwnedCommon {
     /*//////////////////////////////////////////////////////////////
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
@@ -38,47 +17,5 @@ abstract contract HatsOwned {
         HATS = IHats(_hatsContract);
 
         emit OwnerHatUpdated(_ownerHat, _hatsContract);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                             OWNERSHIP LOGIC
-    //////////////////////////////////////////////////////////////*/
-
-    function setOwnerHat(uint256 _ownerHat, address _hatsContract)
-        public
-        virtual
-        onlyOwner
-    {
-        uint256 changes;
-
-        if (ownerHat != _ownerHat) {
-            ownerHat = _ownerHat;
-            // max of 2, so will never overflow
-            unchecked {
-                ++changes;
-            }
-        }
-
-        IHats hats = IHats(_hatsContract);
-
-        if (HATS != hats) {
-            HATS = hats;
-            // max of 2, so will never overflow
-            unchecked {
-                ++changes;
-            }
-        }
-
-        require(changes > 0, "NO CHANGES");
-
-        emit OwnerHatUpdated(_ownerHat, _hatsContract);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                            VIEW FUNCTIONs
-    //////////////////////////////////////////////////////////////*/
-
-    function getHatsContract() public view returns (address) {
-        return address(HATS);
     }
 }
